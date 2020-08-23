c69eef3a2a8d40d73ba921533014dbc4d8e72a1f
Muhibullah-09 <mc2840696@gmail.com>
2020-08-23 09:21:59 +0500

    Update index.js

--- Pure Redux/redux-demo/index.js
+++ Pure Redux/redux-demo/index.js
@@ -1,1 +1,24 @@
-console.log('From index.js')
+//Action defined below
+//Here we define a string constant
+const BUY_CAKE = 'BUY_CAKE';
+
+const BuyCake = () => {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+//Reducer defined below
+//(preState , action ) => newstate 
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+
+        }
+    }
+}
