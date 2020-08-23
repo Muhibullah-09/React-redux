7176bf3eacb35d02e4980d6641c1217e30793c10
Muhibullah-09 <mc2840696@gmail.com>
2020-08-23 12:11:33 +0500

    Update index.js

--- Pure Redux/redux-demo/index.js
+++ Pure Redux/redux-demo/index.js
@@ -1,8 +1,9 @@
 //Here we define react store
 const redux = require('redux');
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
-
+/*
 //Here we define Action 
 // Note:You can see that we have an action creator 'BuyCake' now you could  by all means 
 // pass in the object itself as a parameter to the dispatch method but by having an action creator
@@ -44,4 +45,104 @@ const unSuscribe = store.subscribe(() => console.log('Updated state', store.getS
 store.dispatch(BuyCake())
 store.dispatch(BuyCake())
 store.dispatch(BuyCake())
+unSuscribe()
+
+
+
+//Here we can handle multiple states with one reducer
+//But it's not a good practice for large applications
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
+const BuyCake = () => {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+const BuyIceCream = () => {
+    return {
+        type: BUY_ICECREAM,
+        info: 'second redux action'
+    }
+}
+const initialState = {
+    numOfCakes: 10,
+    numOfIceCreams: 20
+}
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
+        }
+        default: return state
+    }
+}
+const store = createStore(reducer);
+console.log('Initial state', store.getState());
+const unSuscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+store.dispatch(BuyCake())
+store.dispatch(BuyCake())
+store.dispatch(BuyCake())
+store.dispatch(BuyIceCream())
+store.dispatch(BuyIceCream())
+unSuscribe()
+
+*/
+//Best practice is that create multiple state for each task.
+//and create reducer of each state and combine them in a single store with the help of "combineReducer functions"
+const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
+const BuyCake = () => {
+    return {
+        type: BUY_CAKE,
+        info: 'First redux action'
+    }
+}
+const BuyIceCream = () => {
+    return {
+        type: BUY_ICECREAM,
+        info: 'second redux action'
+    }
+}
+const initialCakeState = {
+    numOfCakes: 10
+}
+const initialIceCreamState = {
+    numOfIceCreams: 20
+}
+const cakeReducer = (state = initialCakeState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+        default: return state
+    }
+}
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numOfIceCreams: state.numOfIceCreams - 1
+        }
+        default: return state
+    }
+}
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
+const store = createStore(rootReducer);
+console.log('Initial state', store.getState());
+const unSuscribe = store.subscribe(() => console.log('Updated state', store.getState()));
+store.dispatch(BuyCake())
+store.dispatch(BuyCake())
+store.dispatch(BuyCake())
+store.dispatch(BuyIceCream())
+store.dispatch(BuyIceCream())
 unSuscribe()
