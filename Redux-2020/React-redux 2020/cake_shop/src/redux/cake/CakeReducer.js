e2e64df3668f7e9a3993944a36a06bfcf8ac8942
Muhibullah-09 <mc2840696@gmail.com>
2020-08-24 15:45:28 +0500

    update files

--- Redux-2020/React-redux 2020/cake_shop/src/redux/cake/CakeReducer.js
+++ Redux-2020/React-redux 2020/cake_shop/src/redux/cake/CakeReducer.js
@@ -6,6 +6,7 @@ const cakeReducer = (state = initialState, action) => {
     switch (action.type) {
         case BUY_CAKE:
             return {
+                ...state,
                 numOfCakes: state.numOfCakes - 1
             }
         default:
--- Redux-2020/React-redux 2020/cake_shop/src/redux/iceCream/iceCreamReducer.js
+++ Redux-2020/React-redux 2020/cake_shop/src/redux/iceCream/iceCreamReducer.js
@@ -6,6 +6,7 @@ const iceCreamReducer = (state = initialState, action) => {
     switch (action.type) {
         case BUY_ICECREAM:
             return {
+                ...state,
                 numOfIceCreams: state.numOfIceCreams - 1
             }
         default:
