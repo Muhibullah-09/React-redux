173ec72ea4083a5731d448df3b62f9012846e878
Muhibullah-09 <mc2840696@gmail.com>
2020-09-08 21:37:49 +0500

    code added

--- Redux-2020/Mirage-Api-with-redux/src/redux/counterSlice.js
+++ Redux-2020/Mirage-Api-with-redux/src/redux/counterSlice.js
@@ -2,14 +2,14 @@ import { createSlice } from '@reduxjs/toolkit';
 export const counterSlice = createSlice({
     name: "Counter",
     initialState: {
-        count: 0,
+        count: 0
     },
     reducers: {
         increment: (state) => {
             state.count++
         },
         decrement: (state) => {
-            state.count--
+            state.count--;
         },
         incrementByValue: (state, action) => {
             state.count += action.payload
@@ -17,5 +17,5 @@ export const counterSlice = createSlice({
     },
 });
 
-export const { increment, decrement, incrementByValue } = counterSlice.actions
+export const { increment, decrement, incrementByValue } = counterSlice.actions;
 export default counterSlice.reducer;
--- Redux-2020/Mirage-Api-with-redux/src/redux/store.js
+++ Redux-2020/Mirage-Api-with-redux/src/redux/store.js
@@ -3,5 +3,5 @@ import counterReducer from './counterSlice';
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
-    },
+    }
 }); 
